1a766f2a7288849c11234c1ebbf8a641cb141645
Блиновс <@223_8>
2018-06-05 21:53:53 +0600

    Add project files.

---
+++ Wpf_Module1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf_Module1.Module;
+
+namespace Wpf_Module1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            Helper helper = new Helper();
+            string result = helper.SendMail(txbTo.Text, txbSubject.Text);
+            MessageBox.Show(result);
+        }
+    }
+}
---
+++ Wpf_Module1/Module/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace Wpf_Module1.Module
+{
+    public class Helper
+    {
+        public string SendMail(string to, string subject)
+        {
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("shalun002@gmail.com", "Master Zen");
+
+            msg.To.Add(to);
+            msg.Subject = subject;
+            msg.Body = "Tesb Body";
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("shalun002@gmail.com", "");
+
+            try
+            {
+                smtp.Send(msg);
+                return "Сообщение отправлено!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
